89bfd45c287845f2e3d85cf4067a419d6ecceb9b
Krista Stadler <kristastadler@Kristas-MacBook-Prolocal>
2024-02-14 10:25:51 -0700

    another config update

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,8 +2,8 @@
 # exit on error
 set -o errexit
 
-bundle update
+# bundle update
 bundle install
-# bundle exec rails assets:precompile
-# bundle exec rails assets:clean
+# bundle exec rake assets:precompile
+# bundle exec rake assets:clean
 bundle exec rake db:reset
